4b4e3dd71ed0cd8bb9cff88d522e4663ef36090f
Čajka Vladimír <cajka.vlada@gmail.com>
2024-11-02 07:36:11 +0100

    configuring mobile app, tailwind, eslint, turbo config

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,17 +1,17 @@
 // Learn more: https://docs.expo.dev/guides/monorepos/
 const { getDefaultConfig } = require("expo/metro-config");
 const { FileStore } = require("metro-cache");
-// const { withNativeWind } = require("nativewind/metro");
+const { withNativeWind } = require("nativewind/metro");
 
 const path = require("path");
 
 const config = withTurborepoManagedCache(
-  withMonorepoPaths(getDefaultConfig(__dirname))
-  //   withNativeWind(getDefaultConfig(__dirname), {
-  //     input: "./src/styles.css",
-  //     configPath: "./tailwind.config.ts",
-  //   })
-  // )
+  withMonorepoPaths(
+    withNativeWind(getDefaultConfig(__dirname), {
+      input: "./src/styles/globals.css",
+      configPath: "./tailwind.config.ts",
+    })
+  )
 );
 
 // XXX: Resolve our exports in workspace packages
---
+++ apps/mobile/src/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <>
+      <View className='bg-red-500'>
+        <Text>Welcome!</Text>
+      </View>
+    </>
+  );
+}
