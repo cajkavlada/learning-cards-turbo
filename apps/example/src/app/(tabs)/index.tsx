a176b3e409d2473c1ec7d1027588686d56eeddaa
Čajka Vladimír <cajka.vlada@gmail.com>
2024-11-12 11:09:59 +0100

    adding dependencies to ui test package

--- apps/example/src/app/(tabs)/index.tsx
+++ apps/example/src/app/(tabs)/index.tsx
@@ -4,7 +4,7 @@ import { Text, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { Button, H1, Code } from "@/components";
 import { P } from "@repo/ui";
-// import { TestComponent } from "@repo/ui-mobile";
+import { TestComponent } from "@repo/ui-mobile";
 
 export default function Page() {
   const { top } = useSafeAreaInsets();
@@ -30,9 +30,9 @@ export default function Page() {
             </P>
           </View>
         </View>
-        {/* <TestComponent>
+        <TestComponent>
           <Text>ahoj</Text>
-        </TestComponent> */}
+        </TestComponent>
       </View>
     </View>
   );
