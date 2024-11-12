d4e453b4b827105f867a52d1b5136ef671179fdf
Čajka Vladimír <cajka.vlada@gmail.com>
2024-11-13 00:56:00 +0100

    final ts issues

--- packages/backend-core/src/test/index.ts
+++ packages/backend-core/src/test/index.ts
@@ -1,11 +1,11 @@
 import "server-only";
 
 export async function getTitle() {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
+  await new Promise<void>((resolve) => setTimeout(resolve, 1000));
   return "Welcome!";
 }
 
 export async function sayHi(name: string) {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
+  await new Promise<void>((resolve) => setTimeout(resolve, 1000));
   return `Hi, ${name}!`;
 }
